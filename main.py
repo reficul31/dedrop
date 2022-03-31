521bfc046be6fe54799f2c44b20d09aee88dd270
reficul31 <reficul31@gmail.com>
2022-03-31 09:54:05 -0400

    Adding batch size argument to the trainer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import torch
 
-from .ssim import SSIM
+from ssim import SSIM
 from models import PReNet
 from torch.optim import Adam
 from torch.utils.data import DataLoader
@@ -12,16 +12,18 @@ from train import Trainer
 if __name__ == '__main__':
     name = 'prenet'
     root_dir = "/home/sb4539/dedrop"
+    
+    epochs, batch_size = 100, 16
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = PReNet().to(device)
     
     train_dataset = get_dataset(root_dir, phase='train')
-    dataloader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+    dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
     criterion = SSIM()
     optimizer = Adam(model.parameters(), lr=1e-3)
     scheduler = MultiStepLR(optimizer, milestones=[30, 50, 80])
 
-    trainer = Trainer(criterion, optimizer, scheduler, dataloader, root_dir)
-    model, _, _ = trainer.train_model(name, model, epochs=100)
+    trainer = Trainer(criterion, optimizer, scheduler, dataloader, root_dir, batch_size)
+    model, _, _ = trainer.train_model(name, model, epochs=epochs)
