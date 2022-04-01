97b6af9e3f13617f6eb9db5229fc4953b69aa6dc
reficul31 <reficul31@gmail.com>
2022-03-31 21:09:33 -0400

    Adding data augmentation techniques

--- dataset.py
+++ dataset.py
@@ -1,15 +1,19 @@
 import os
+import numpy as np
 
 from PIL import Image
 from torch.utils.data import Dataset
-from torchvision.transforms import Compose, Resize, ToTensor
+from torchvision.transforms import Compose, ToTensor
 
 class DedropDataset(Dataset):
-    def __init__(self, root_dir, phase = 'train', transform=None):
+    def __init__(self, root_dir, phase, transform=None, normalize = True, size=(480, 760), window=128):
         super().__init__()
         
+        self.size = size
         self.phase = phase
         self.transform = transform
+        self.window = window
+        self.normalize = normalize
 
         self.rain_folder_path = os.path.join(root_dir, phase, "data")
         self.clean_folder_path = os.path.join(root_dir, phase, "gt")
@@ -25,13 +29,26 @@ class DedropDataset(Dataset):
     def __len__(self):
         return len(self.rain_image_list)
     
+    def get_random_patches(self, rain, clean):
+        endh, endw = self.size
+        width, height = np.random.randint(0, endw - self.window-1), np.random.randint(0, endh - self.window-1)
+
+        rain_patch = rain[width: width+self.window, height: height+self.window, :]
+        clean_patch = clean[width: width+self.window, height: height+self.window, :]
+        return rain_patch, clean_patch
+    
     def __getitem__(self, idx):
         clean_image_path = os.path.join(self.clean_folder_path, self.clean_image_list[idx])
         rain_image_path = os.path.join(self.rain_folder_path, self.rain_image_list[idx])
         
-        rain = Image.open(rain_image_path).convert("RGB")
-        clean = Image.open(clean_image_path).convert("RGB")
+        rain = Image.open(rain_image_path).resize(self.size).convert("RGB")
+        clean = Image.open(clean_image_path).resize(self.size).convert("RGB")
 
+        rain, clean = self.get_random_patches(np.array(rain), np.array(clean))
+        
+        if self.normalize:
+            rain, clean = rain / 255, clean / 255
+        
         if self.transform is None:
             return rain, clean
 
@@ -42,8 +59,5 @@ def get_dataset(root_dir, phase):
     return DedropDataset(
         root_dir, 
         phase=phase, 
-        transform=Compose([
-            Resize((240, 360)),
-            ToTensor()
-        ])
+        transform=Compose([ToTensor()])
     )
