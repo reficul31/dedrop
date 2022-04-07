36f1c0bb1006a5c6fd17e4e0e704cca856784b2e
reficul31 <reficul31@gmail.com>
2022-04-07 18:10:23 -0400

    Add code for training vae

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,2 @@
 from .prenet import *
+from .vae import *
--- train.py
+++ train.py
@@ -11,6 +11,60 @@ class Trainer:
         self.data_loader = data_loader
         self.root_dir = root_dir
         self.batch_size = batch_size
+    
+    def train_vae(self, name, model, epochs=100, checkpoint_epoch=0, save_checkpoint_frequency=20, print_frequency=15):
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        
+        if checkpoint_epoch != 0:
+            print("Continuing training from epoch = {}".format(checkpoint_epoch))
+        
+        train_loss = []
+        for epoch in range(checkpoint_epoch, epochs):
+            model.train()
+            batch_step_size = len(self.data_loader.dataset) / self.batch_size
+            
+            log_loss = []
+            start = time.time()
+            for batch_idx, sample in enumerate(self.data_loader):
+                rain, clean = sample
+                rain = rain.to(device).float()
+                clean = clean.to(device).float()
+                outputs, _, _ = model(rain)
+                loss = self.criterion(outputs, clean)
+
+                with torch.set_grad_enabled(True):
+                    self.optimizer.zero_grad()
+                    loss.backward()
+                    self.optimizer.step()
+                    self.scheduler.step()
+
+                log_loss.append(loss.item())
+                if batch_idx % print_frequency == 0:
+                    print("Epoch {} : {} ({:04d}/{:04d}) Loss = {:.4f}".format(epoch + 1, 'Train', batch_idx, int(batch_step_size), loss.item()))
+            
+            train_loss.append(np.mean(log_loss))
+            print("Epoch {} done: Time = {}, Mean Loss = {}".format(epoch + 1, time.time() - start, train_loss[-1]))
+            
+            if not os.path.isdir(os.path.join(self.root_dir, name)):
+                os.makedirs(os.path.join(self.root_dir, name))
+
+            if epoch % save_checkpoint_frequency == 0:
+                torch.save({
+                    'epoch': epoch,
+                    'model_state_dict': model.state_dict(),
+                    'optimizer_state_dict': self.optimizer.state_dict(),
+                    'scheduler_state_dict': self.scheduler.state_dict()
+                }, os.path.join(self.root_dir, name, "checkpoint_{}.tar".format(epoch)))
+                np.save(os.path.join(self.root_dir, name, "train-loss-epoch-{}.npy".format(epoch)), train_loss)
+            else:
+                torch.save({
+                    'epoch': epoch,
+                    'model_state_dict': model.state_dict(),
+                    'optimizer_state_dict': self.optimizer.state_dict(),
+                    'scheduler_state_dict': self.scheduler.state_dict()
+                }, os.path.join(self.root_dir, name, "checkpoint_latest.tar"))
+        return model, train_loss
+
 
     def train_model(self, name, model, epochs=100, checkpoint_epoch = 0, save_checkpoint_frequency=20, print_frequency=15):
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -65,3 +119,6 @@ class Trainer:
                     'scheduler_state_dict': self.scheduler.state_dict()
                 }, os.path.join(self.root_dir, name, "checkpoint_latest.tar"))
         return model, train_loss
+
+if __name__ == "__main__":
+    print("This is not the training file. Please run one of the train model files.")
