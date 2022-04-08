232e7051848f38733a2463bd3d9642e3063a08fb
reficul31 <reficul31@gmail.com>
2022-04-07 20:56:49 -0400

    Installing gdown using conda

--- get_dataset.sh
+++ get_dataset.sh
@@ -1,4 +1,4 @@
-pip install gdown
+conda install -c conda-forge gdown
 gdown https://drive.google.com/drive/folders/1e7R76s6vwUJxILOcAsthgDLPSnOrQ49K -O /home/sb4539/dedrop --folder
 unzip train.zip
 unzip test_a.zip
